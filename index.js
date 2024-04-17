1815bcb9335141450ddeed4ab6b11c6f168e225e
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-04-17 10:09:16 -0400

    Atualizado 16/04
    Arrumei alguns erros no código

--- index.js
+++ index.js
@@ -2,36 +2,27 @@ require("dotenv").config();
 const conn = require("./db/conn");
 
 const Usuario = require("./models/Usuario");
-
 const express = require("express");
 const app = express();
 
-app.use(
-    express.urlencoded ({
-        extended: true,
-    })
-);
-
-app.use(express.json());
-
-app.get("usuarios/novo", (req, res) => {
+app.get("/usuarios/novo", (req,res) =>{
     res.sendFile(`${__dirname}/views/formUsuario.html`);
 });
 
-app.post("/usuarios/novo", async (req, res) => {
+app.post("/usuarios/novo", (req,res) =>{
     const nickname = req.body.nickname;
-    const nome = req.body.nome;
+    const name  = req.body.nome;
 
     const dadosUsuario = {
         nickname,
         nome,
     };
-    const usuario = await Usuario.create(dadosUsuario);
+    const usuario = Usuario.create(dadosUsuario);
 
-    res.send("Usuário inserido sob o id " + usuario.id);
+    res.send("Usuario inserido sob o id " + usuario.id);
 });
 
-app.listen(3000);
+app.listen(8000);
 
 conn
 .sync()
